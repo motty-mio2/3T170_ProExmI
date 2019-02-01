d7e472656cb94ca14bc2800978fb3f8eec035e58
motty-mio2 <motty.mio2@gmail.com>
2019-02-01 10:14:49 +0900

    A-2 (fin)

--- Challeng4Q/A-2.c
+++ Challeng4Q/A-2.c
@@ -7,7 +7,7 @@ int main () {
     do {
     printf("西暦（0以上）を入力してください：");
     scanf("%d",&inyear);
-    } while(&inyear < 0);
+    } while(inyear < 0);
 
     if (inyear % 4 == 0 && (inyear % 100 != 0 || inyear % 400 == 0)) {
         printf("うるう年です。\n");
